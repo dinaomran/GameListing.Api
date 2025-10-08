6382446640483ddf62ddb8d0296c020167b8bab1
Dina <dinaomran8@gmail.com>
2025-10-08 17:54:59 +0300

    Add project files.

---
+++ GameListing.Api/Data/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GameListing.Api.Data;
+
+public class Game
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public DateOnly ReleaseDate { get; set; }
+    public double Price { get; set; }
+}
